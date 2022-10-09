4359671cf97d925a1a7b2a28af95f677b6973139
hieunguyen29 <hieunmph27066@fpt.edu.vn>
2022-10-09 23:56:33 +0700

    Update login.php

--- site/login.php
+++ site/login.php
@@ -9,7 +9,6 @@ include_once '../global.php'
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="../content/css/css.css">
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
