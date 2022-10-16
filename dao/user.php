a8d655c8d5eedd00618958d637dbeb54fbb21644
hieunguyen29 <hieunmph27066@fpt.edu.vn>
2022-10-17 01:07:09 +0700

    sửa dao đổ data đủ

--- dao/user.php
+++ dao/user.php
@@ -1,26 +1,19 @@
 <?php
-include_once 'pdo.php';
+include 'pdo.php';
 
-function user_insert($email, $mat_khau, $address,$phone,  $role_id, $ho_ten){
-    $sql = "INSERT INTO user(id,email, password,address_number, phone, ,role_id,name) VALUES (null,?, ?, ?, ?, ?, ?)";
-    pdo_execute($sql,null,$email, $mat_khau,$address,$phone,   $role_id, $ho_ten);
+function user_insert($id, $email, $password, $address, $phone_number, $role_id){
+    $sql = "INSERT INTO user(id, email, 'password', 'address', phone_number, role_id) VALUES (?, ?, ?, ?, ?, ?)";
+    pdo_execute($sql, $id, $email, $password, $address, $phone_number, $role_id);
 }
 
-function user_update($ma_kh, $mat_khau, $ho_ten, $email, $phone, $address,$role_id){
-    $sql = "UPDATE user SET mat_khau=?,ho_ten=?,email=?,hinh=?,kich_hoat=?,vai_tro=? WHERE ma_kh=?";
-    pdo_execute($sql,$ma_kh, $mat_khau, $ho_ten, $email, $phone, $address,$role_id);
+function user_update($id, $email, $password, $address, $phone_number, $role_id){
+    $sql = "UPDATE user SET email=?, 'password'=?, 'address'=?, phone_number=?, role_id=? WHERE id=?";
+    pdo_execute($sql,$id, $email, $password, $address, $phone_number, $role_id);
 }
 
-function user_delete($ma_kh){
-    $sql = "DELETE FROM user  WHERE ma_kh=?";
-    if(is_array($ma_kh)){
-        foreach ($ma_kh as $ma) {
-            pdo_execute($sql, $ma);
-        }
-    }
-    else{
-        pdo_execute($sql, $ma_kh);
-    }
+function user_delete($id){
+    $sql = "DELETE FROM user  WHERE id=?";
+        pdo_execute($sql, $id);
 }
 
 function user_select_all(){
@@ -28,14 +21,14 @@ function user_select_all(){
     return pdo_query($sql);
 }
 
-function user_select_by_id($ma_kh){
-    $sql = "SELECT * FROM user WHERE ma_kh=?";
-    return pdo_query_one($sql, $ma_kh);
+function user_select_by_id($id){
+    $sql = "SELECT * FROM user WHERE id=?";
+    return pdo_query_one($sql, $id);
 }
 
-function user_exist($ma_kh){
-    $sql = "SELECT count(*) FROM user WHERE $ma_kh=?";
-    return pdo_query_value($sql, $ma_kh) > 0;
+function user_exist($id){
+    $sql = "SELECT count(*) FROM user WHERE $id=?";
+    return pdo_query_value($sql, $id) > 0;
 }
 
 function user_select_by_role($role_id){
@@ -43,7 +36,7 @@ function user_select_by_role($role_id){
     return pdo_query($sql, $role_id);
 }
 
-function user_change_password($ma_kh, $mat_khau_moi){
-    $sql = "UPDATE user SET mat_khau=? WHERE ma_kh=?";
-    pdo_execute($sql, $mat_khau_moi, $ma_kh);
+function user_change_password($id, $mat_khau_moi){
+    $sql = "UPDATE user SET 'password'=? WHERE id=?";
+    pdo_execute($sql, $mat_khau_moi, $id);
 }
